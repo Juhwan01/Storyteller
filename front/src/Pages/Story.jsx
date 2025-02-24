9af1ace917b1aa51474983b6f22415afc1ef01a5
Juhwan01 <wnghks5432@naver.com>
2025-02-24 15:48:17 +0900

    02.24 - IPv4 주소 변경

--- front/src/Pages/Story.jsx
+++ front/src/Pages/Story.jsx
@@ -27,7 +27,7 @@ const Story = () => {
         }
 
         try {
-          const response = await fetch("http://13.209.64.66:8000/story/check-location", {
+          const response = await fetch("http://13.124.134.6:8000/story/check-location", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
